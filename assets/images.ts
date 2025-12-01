f015cae9512247d2ae79b6b3ae60b47003ea8dd0
lastkitsune17 <dwifanirizki@gmail.com>
2025-12-01 08:55:59 +0700

    fix(images): Correct Naura's team image URL
    The image URL for the team member 'Naura' was incorrect and pointed to the same image as 'Farhan'. This commit updates the URL to the correct image asset for Naura.

--- assets/images.ts
+++ assets/images.ts
@@ -27,7 +27,7 @@ export const IMAGES = {
     FARHAN: "https://i.postimg.cc/B8jphyF6/IMG-20251130-WA0032.jpg",
     INGANATUS: "https://i.postimg.cc/RWsGL9WH/IMG-20251130-WA0029",
     ANGGUN: "https://i.postimg.cc/tsSDNysQ/IMG-20251130-WA0031.jpg",
-    NAURA: "https://i.postimg.cc/B8jphyF6/IMG-20251130-WA0032.jpg",
+    NAURA: "https://i.postimg.cc/PC3KQhC8/IMG-20251130-WA0030.jpg",
     RIZKI: "https://i.postimg.cc/MnPtmxnB/IMG-20251130-WA0033.jpg",
   },
   
